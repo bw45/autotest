01b904949245ea608fadaa179ed1a50c5a32c96b
bo.wu <185356866@qq.com>
2019-09-03 20:37:41 +0800

    easyAutoTest framework version1.0

---
+++ auto/case/check.py
@@ -0,0 +1,13 @@
+#初始化webdriver
+import time
+from bs4 import BeautifulSoup
+from auto.setting import Setting
+
+#检查各项联动配置
+class Check(Setting):
+	def example1(self):
+		#系统类检查用例,可自定义或者省略
+		pass
+	def example2(self):
+		#系统类检查用例,可自定义或者省略
+		pass
